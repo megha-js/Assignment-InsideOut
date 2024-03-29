9a43c3950c5cdf6d24f267f586592c197f7039de
megha-js <130333862+megha-js@users.noreply.github.com>
2024-03-30 01:54:40 +0530

    Update globalContext.js

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:4000/api/v1/";
+const BASE_URL = "https://assignment-inside-out.vercel.app/api/v1/";
 
 
 const GlobalContext = React.createContext()
